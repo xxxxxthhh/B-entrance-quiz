375e8c7d7a73ac4a76b0508da8c5bc501e0902a7
KyleXu <kyle_x@foxmail.com>
2020-10-27 21:40:49 +0800

    [XTH]: add crossOrigin And Origin filter

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
@@ -1,8 +1,23 @@
 package com.thoughtworks.capability.gtb.entrancequiz.api;
 
+import com.thoughtworks.capability.gtb.entrancequiz.dto.Student;
+import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
+  @Autowired
+  StudentService studentService;
 
+  @GetMapping("/students")
+  @CrossOrigin(origins = "http://localhost:1234")
+  public ResponseEntity<List<Student>> getStudentsList(){
+    return ResponseEntity.ok().body(studentService.getStudentsList());
+  }
 }
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentService.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.capability.gtb.entrancequiz.service;
 
+import com.thoughtworks.capability.gtb.entrancequiz.dto.Student;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
-
+  public List<Student> getStudentsList(){
+    return null;
+  }
 }
